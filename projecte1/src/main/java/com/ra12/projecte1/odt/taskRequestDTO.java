ce32acd1321f0d43a5550a9da9cde6b925002d1e
IvanGoMa <99ivangm@gmail.com>
2026-01-21 19:58:08 +0100

    dto

--- projecte1/src/main/java/com/ra12/projecte1/odt/taskRequestDTO.java
+++ projecte1/src/main/java/com/ra12/projecte1/odt/taskRequestDTO.java
@@ -1,8 +1,18 @@
 package com.ra12.projecte1.odt;
 
+import java.sql.Timestamp;
+
+import com.ra12.projecte1.model.Task;
+
 public class taskRequestDTO {
     private String nomTaska; 
     private int sparks;
+    private Timestamp dataLimit;
     private String urlImage;
+
+    public Task toTask(){
+        return new Task(nomTaska, sparks, dataLimit, new Timestamp(System.currentTimeMillis()), new Timestamp(System.currentTimeMillis()), urlImage);
+    }
+
 }
 
--- projecte1/src/main/java/com/ra12/projecte1/odt/taskResponseDTO.java
+++ projecte1/src/main/java/com/ra12/projecte1/odt/taskResponseDTO.java
@@ -1,8 +1,19 @@
 package com.ra12.projecte1.odt;
 
+import java.sql.Timestamp;
+
 public class taskResponseDTO {
     private Long id; 
     private String nomTaska; 
     private int sparks;
+    private Timestamp dataLimit;
     private String urlImage;
+
+    @Override
+    public String toString() {
+        return "Tasca [id=" + id + ", nomTaska=" + nomTaska + ", sparks=" + sparks + ", urlImage=" + urlImage
+                + "]";
+    }
+
+    
 }
